9546d5228de6cb950706c0744d2a5f6cd2f5d700
Iñigo Mediavilla Saiz <inigo.mediavillasaiz@doctolib.com>
2025-01-24 16:30:27 +0100

    Print table name on seq_scan (#2)
    # Changes
    
    - Cleaner way to get reloid. If node type = SeqScan we cast to seqscan
    and we get the scanrelid from it.
    - From the rtables we can convert a scanrelid not only to a reloid but
    also to the table name (to validate tomorrow if this also works for
    things that are not real relations)
    
    # Example of running session
    
    ```
    psql (15.10)
    Type "help" for help.
    
    pg_no_seqscan=# select * from foo where id < 10;
    NOTICE:  T_SeqScan on table: 'foo'
     id
    ----
      1
      2
      3
      4
      5
      6
      7
      8
      9
    (9 rows)
    ```

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,73 +1,36 @@
-use pgrx::pg_sys::NodeTag::T_SeqScan;
-use pgrx::pg_sys::Plan;
+use std::ffi::CStr;
+use pgrx::pg_sys::{get_rel_name, rt_fetch, CmdType::CMD_UTILITY, List, NodeTag::T_SeqScan, Plan, SeqScan};
 #[allow(deprecated)]
 use pgrx::{pg_sys, register_hook, HookResult, PgBox, PgHooks};
 use pgrx::notice;
-use regex::Regex;
 
 struct NoSeqscanHooks;
 
-fn notice_on_seq_scans(plan: &Plan) {
-    // See Plan documentation: https://github.com/postgres/postgres/blob/master/src/include/nodes/plannodes.h#L119
-    let (left_ref, right_ref, _target_list) = unsafe {
-        (
-            plan.lefttree.as_ref(),
-            plan.righttree.as_ref(),
-            plan.targetlist.as_ref(),
-        )
-    };
+unsafe fn resolve_table_name(rtables: *mut List, scanrelid: u32) -> String {
+    let relname = get_rel_name(rt_fetch(scanrelid, rtables).as_ref().unwrap().relid);
+    let c_str: &CStr = unsafe { CStr::from_ptr(relname) };
+    c_str.to_str().unwrap().to_string()
+}
 
-    match left_ref {
-        Some(left_plan) => {
-            notice_on_seq_scans(left_plan);
-        }
-        None => {}
+fn notice_on_seq_scans(plan: *mut Plan, rtables: *mut List) {
+    unsafe {
+        plan.as_ref().map(|plan_ref| {
+            if plan_ref.type_ == T_SeqScan {
+                let seq_scan: &mut SeqScan = &mut *(plan as *mut SeqScan);
+                notice!(
+                    "{:?} on table: '{}'",
+                    plan_ref.type_,
+                    resolve_table_name(rtables, seq_scan.scan.scanrelid)
+                );
+            } else {
+                // See Plan documentation: https://github.com/postgres/postgres/blob/master/src/include/nodes/plannodes.h#L119
+                notice_on_seq_scans(plan_ref.lefttree, rtables);
+                notice_on_seq_scans(plan_ref.righttree, rtables);
+            }
+        });
+
     }
 
-    match right_ref {
-        Some(right_plan) => {
-            notice_on_seq_scans(right_plan);
-        }
-        None => {}
-    }
-
-    if plan.type_ != T_SeqScan {
-        return;
-    }
-
-    let targetlist = unsafe { plan.targetlist.as_ref() };
-    let target_string = targetlist.map_or(String::from(""), |t| t.to_string());
-
-    // TODO find oid with an other strategy
-    // Parsing something like
-    // "({TARGETENTRY :expr {VAR :varno 1 :varattno 1 :vartype 23 :vartypmod -1 :varcollid 0 :varlevelsup 0 :varnosyn 1 :varattnosyn 1 :location 7} :resno 1 :resname id :ressortgroupref 0 :resorigtbl 16398 :resorigcol 1 :resjunk false})"
-    let re = Regex::new(r":resorigtbl (?<resorigtabl>\d+) :").unwrap();
-    let Some(result) = re.captures(&target_string) else {
-        return;
-    };
-
-    notice!(
-        "{:?} on table with oid: {}",
-        plan.type_,
-        &result["resorigtabl"],
-    );
-
-    /*
-    notice!(
-        "[#{:?}]({:?}) something: {:?}",
-        plan.plan_node_id,
-        plan.type_,
-        targetlist.map_or(String::from(""), |target_list| format!(
-            "{:?} {:?}",
-            unsafe { target_list.elements.as_ref().unwrap().oid_value },
-            // target_list.type_,
-            // target_list.length,
-            unsafe { (*pgrx::pg_sys::list_head(target_list)).oid_value },
-            // unsafe { (*target_list.elements.wrapping_add(0)).oid_value }
-            //unsafe { (*pgrx::pg_sys::list_head(target_list) as TargetEntry).resorigtbl },
-        ))
-    );
-    */
 }
 
 #[allow(deprecated)]
@@ -80,19 +43,12 @@ impl PgHooks for NoSeqscanHooks {
     ) -> HookResult<()> {
         // See PlannedStmt documentation: https://github.com/postgres/postgres/blob/master/src/include/nodes/plannodes.h#L46
         unsafe {
-            match query_desc
+            query_desc
                 .plannedstmt
                 .as_ref()
-                .and_then(|ps| ps.planTree.as_ref())
-            {
-                Some(plan_ref) => notice_on_seq_scans(plan_ref),
-                _ => {}
-            }
-        };
-        /*notice!(
-            "            executor_start HOOK, {:?}",
-            format!("query_desc: {}", unsafe { *query_desc.plannedstmt })
-        );*/
+                .map (|ps| notice_on_seq_scans(ps.planTree, ps.rtable));
+
+        }
         prev_hook(query_desc, eflags)
     }
 }
