cb6406646da7b0c92187e6abe058f9450da04ef0
Alice Loeser <alice.loeser@gmail.com>
2025-01-27 10:22:45 +0100

    notice on seq scan only on select queries (#3)
    ![explain](https://github.com/user-attachments/assets/5ab3f88e-d7cb-493b-8e60-9a315dceb654)
    
    ---------
    
    Co-authored-by: Inigo Mediavilla Saiz <inigo.mediavillasaiz@doctolib.com>

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,8 +1,9 @@
-use std::ffi::CStr;
-use pgrx::pg_sys::{get_rel_name, rt_fetch, CmdType::CMD_UTILITY, List, NodeTag::T_SeqScan, Plan, SeqScan};
+use pgrx::notice;
+use pgrx::pg_sys::CmdType;
+use pgrx::pg_sys::{get_rel_name, rt_fetch, List, NodeTag::T_SeqScan, Plan, SeqScan};
 #[allow(deprecated)]
 use pgrx::{pg_sys, register_hook, HookResult, PgBox, PgHooks};
-use pgrx::notice;
+use std::ffi::CStr;
 
 struct NoSeqscanHooks;
 
@@ -12,25 +13,24 @@ unsafe fn resolve_table_name(rtables: *mut List, scanrelid: u32) -> String {
     c_str.to_str().unwrap().to_string()
 }
 
-fn notice_on_seq_scans(plan: *mut Plan, rtables: *mut List) {
+fn notice_on_seq_scans(plan: *mut Plan, rtables: *mut List, query_string: &String) {
     unsafe {
         plan.as_ref().map(|plan_ref| {
             if plan_ref.type_ == T_SeqScan {
                 let seq_scan: &mut SeqScan = &mut *(plan as *mut SeqScan);
                 notice!(
-                    "{:?} on table: '{}'",
+                    "{:?} on table: '{}' - query: '{}'",
                     plan_ref.type_,
-                    resolve_table_name(rtables, seq_scan.scan.scanrelid)
+                    resolve_table_name(rtables, seq_scan.scan.scanrelid),
+                    query_string
                 );
             } else {
                 // See Plan documentation: https://github.com/postgres/postgres/blob/master/src/include/nodes/plannodes.h#L119
-                notice_on_seq_scans(plan_ref.lefttree, rtables);
-                notice_on_seq_scans(plan_ref.righttree, rtables);
+                notice_on_seq_scans(plan_ref.lefttree, rtables, query_string);
+                notice_on_seq_scans(plan_ref.righttree, rtables, query_string);
             }
         });
-
     }
-
 }
 
 #[allow(deprecated)]
@@ -42,13 +42,25 @@ impl PgHooks for NoSeqscanHooks {
         prev_hook: fn(query_desc: PgBox<pg_sys::QueryDesc>, eflags: i32) -> HookResult<()>,
     ) -> HookResult<()> {
         // See PlannedStmt documentation: https://github.com/postgres/postgres/blob/master/src/include/nodes/plannodes.h#L46
-        unsafe {
-            query_desc
-                .plannedstmt
-                .as_ref()
-                .map (|ps| notice_on_seq_scans(ps.planTree, ps.rtable));
+        let query_string = unsafe { CStr::from_ptr(query_desc.sourceText) }
+            .to_str()
+            .unwrap()
+            .to_string()
+            .to_lowercase();
 
+        if query_desc.operation == CmdType::CMD_SELECT {
+            let query_first_word = query_string.split_whitespace().next().unwrap_or("");
+
+            if query_first_word != "explain" {
+                unsafe {
+                    query_desc
+                        .plannedstmt
+                        .as_ref()
+                        .map(|ps| notice_on_seq_scans(ps.planTree, ps.rtable, &query_string));
+                }
+            }
         }
+
         prev_hook(query_desc, eflags)
     }
 }
