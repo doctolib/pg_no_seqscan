0cba5fcccac7d782f5b81f7e868e67265f1961c7
Emmanuel Quincerot <emmanuel.quincerot@doctolib.com>
2025-10-04 15:47:01 +0200

    remove pg13 support

--- src/hooks.rs
+++ src/hooks.rs
@@ -143,9 +143,9 @@ Query: {}
         }
 
         let seq_scan: &mut SeqScan = &mut *(node as *mut SeqScan);
-        #[cfg(not(any(feature = "pg13", feature = "pg14")))]
+        #[cfg(not(any(feature = "pg14")))]
         let table_oid = scanned_table(seq_scan.scan.scanrelid, rtables).unwrap();
-        #[cfg(any(feature = "pg13", feature = "pg14"))]
+        #[cfg(any(feature = "pg14"))]
         let table_oid = scanned_table(seq_scan.scanrelid, rtables).unwrap();
 
         if self.is_sequence(table_oid) {
@@ -216,7 +216,7 @@ Query: {}
                     self.check_query(&query_desc);
                 }
             }
-            #[cfg(not(any(feature = "pg13", feature = "pg14")))]
+            #[cfg(not(any(feature = "pg14")))]
             CmdType::CMD_MERGE => {
                 if !is_explain_stmt && !self.is_ignored_user(unsafe { current_username() }) {
                     self.check_query(&query_desc);
