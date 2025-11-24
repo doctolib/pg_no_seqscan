9e60b0e19c14ca9eadf2f8643ca8979cf4429d5e
Iñigo Mediavilla Saiz <inigo.mediavillasaiz@doctolib.com>
2025-11-24 11:11:38 +0100

    Update tests/pg_regress/sql/explain.sql
    Co-authored-by: Emmanuel Quincerot <emmanuel.quincerot@doctolib.com>

--- tests/pg_regress/sql/explain.sql
+++ tests/pg_regress/sql/explain.sql
@@ -15,7 +15,7 @@ EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF) SELECT * FROM
 -- Allows query execution as it is an EXPLAIN ANALYZE, even for parallel queries
 SET parallel_setup_cost = 0;
 SET parallel_tuple_cost = 0.000001;
-CREATE TABLE test_explain_parallel (id, migrated_at) AS (SELECT generate_series(1, 800000)::bigint id, null::timestamp without time zone);
+CREATE TABLE test_explain_parallel (id, migrated_at) AS (SELECT generate_series(1, 800000)::bigint id, null::text);
 EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF) select count(*) from test_explain_parallel;
 SET parallel_setup_cost TO DEFAULT;
 SET parallel_tuple_cost TO DEFAULT;
