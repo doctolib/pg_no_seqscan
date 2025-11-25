dd75576089923a9944e84d17c8970fd44121fa49
Iñigo Mediavilla Saiz <inigo.mediavillasaiz@doctolib.com>
2025-11-25 10:18:30 +0100

    fix(#26): Fix explain blocked in parallel seqscan (#29)
    Fixes(#26)
    
    Additional benefits:
    
    - Since the new approach detects an explain statement thanks to `eflags`
    and `query_desc.instrument_options`, we no longer need to keep state
    between hooks to know if a query is an explain statement.
    
    ---------
    
    Co-authored-by: Emmanuel Quincerot <emmanuel.quincerot@doctolib.com>

--- tests/pg_regress/sql/explain.sql
+++ tests/pg_regress/sql/explain.sql
@@ -12,8 +12,19 @@ SELECT * FROM test_explain;
 -- Allows query execution as it is an EXPLAIN ANALYZE, as it may be useful to understand the plan
 EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF) SELECT * FROM test_explain;
 
+-- Allows query execution as it is an EXPLAIN ANALYZE, even for parallel queries
+SET parallel_setup_cost = 0;
+SET parallel_tuple_cost = 0.000001;
+CREATE TABLE test_explain_parallel (id, migrated_at) AS (SELECT generate_series(1, 800000)::bigint id, null::text);
+EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF) select count(*) from test_explain_parallel;
+SET parallel_setup_cost TO DEFAULT;
+SET parallel_tuple_cost TO DEFAULT;
+
 -- Blocks query execution as this is the real query
 SELECT * FROM test_explain;
 
+
 -- cleanup
 drop table test_explain;
+drop table test_explain_parallel;
+
